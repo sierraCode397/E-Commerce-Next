f40f2ba3e88e16254f28f7cc99732a64d0384cc7
izaack107 <izaack107@gmail.com>
2023-03-08 23:52:17 -0600

    Se agrego google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,10 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>E-Commerce</title>
+
+      </Head>
       <body>
         <Main />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DN9FGTX7KE"></script>
+        <script dangerouslySetInnerHTML={{
+          html: `  window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());    
+          gtag('config', 'G-DN9FGTX7KE');`
+        }} />
         <NextScript />
       </body>
     </Html>
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react';
+import Head from "next/head";
 import OrderItem from '@components/OrderItem';
 import AppContext from '@context/AppContext';
 import styles from '@styles/Checkout.module.scss';
@@ -7,23 +8,28 @@ const Checkout = () => {
 	const {state, sumTotal, formattedDate} = useContext(AppContext)
 
 	return (
-		<div className= {styles.Checkout}>
-			<div className={styles["Checkout-container"]}>
-				<h1 className={styles["title-checkout"]}>My order checkout</h1>
-				<div className={styles["Checkout-content"]}>
-					<div className={styles["order-checkout"]}>
-						<p>
-							<span>{formattedDate}</span>
-							<span>{state.cart.length} Articulos</span>
-						</p>
-						<p>${sumTotal()}</p>
+		<>
+			<Head>
+				<title>Checkout</title>
+			</Head>
+			<div className= {styles.Checkout}>
+				<div className={styles["Checkout-container"]}>
+					<h1 className={styles["title-checkout"]}>My order checkout</h1>
+					<div className={styles["Checkout-content"]}>
+						<div className={styles["order-checkout"]}>
+							<p>
+								<span>{formattedDate}</span>
+								<span>{state.cart.length} Articulos</span>
+							</p>
+							<p>${sumTotal()}</p>
+						</div>
 					</div>
+					{state.cart.map(product => (
+							<OrderItem product={product} key={`orderItem-${product.id}`}/>
+					))}
 				</div>
-				{state.cart.map(product => (
-						<OrderItem product={product} key={`orderItem-${product.id}`}/>
-				))}
 			</div>
-		</div>
+		</>
 	);
 }
 
